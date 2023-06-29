85cb3858bc21f1566451f008430cf247da7c093d
hkatkam559 <130535353+hkatkam559@users.noreply.github.com>
2023-06-29 17:29:34 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,3 +2,5 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 yum install java* -y
+systemctl start jenkins.service
+systemctl status jenkins.service
