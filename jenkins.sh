2cef7cf963679c034d479bb2692dc9890a03e839
EC2 Default User <ec2-user@ip-172-31-40-157.ap-south-1.compute.internal>
2023-06-29 08:01:03 +0000

    jenkins commands

---
+++ jenkins.sh
@@ -0,0 +1,4 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+yum install java* -y
